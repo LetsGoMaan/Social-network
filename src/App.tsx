f4a47fc1e7b46da10d7713bc88ea4e3c77769056
LetsGoMan <ilikeseik@gmail.com>
2023-03-07 21:58:03 +0300

    10. The danger of default export and auto export, Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import {Header} from "./Header";
 import {Techologies} from "./Techologies";
+import {Footer} from "./Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Techologies/>
+            <Footer/>
         </div>
     );
 }
