0a42be1b259ec87c1beea6be0abe5e0e298e1ce4
LetsGoMan <ilikeseik@gmail.com>
2023-03-11 21:44:16 +0300

    30. Repetition of the theory

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Header} from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
@@ -41,11 +41,9 @@ export type AppPropsType = {
 
 function App(props: AppPropsType) {
     return (
-        <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <NavBar/>
-
             <div className="app-wrapper-content">
                 <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage}/>}/>
                 <Route path="/profile" render={() => <Profile state={props.state.profilePage}/>}/>
@@ -53,9 +51,7 @@ function App(props: AppPropsType) {
                 <Route path="/music" render={() => <Music/>}/>
                 <Route path="/settings" render={() => <Settings/>}/>
             </div>
-
         </div>
-        </BrowserRouter>
     );
 }
 
