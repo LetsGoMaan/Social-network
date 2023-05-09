3e05666db4aca9fa80ff8f919d3e3d5aedd1afa8
LetsGoMan <ilikeseik@gmail.com>
2023-05-09 12:56:12 +0300

    75.1 Refactoring

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,31 +1,85 @@
 import React from "react";
+import {SubmitHandler, useForm} from "react-hook-form"
+
+
+type InputsType = {
+    login: string,
+    password: string,
+    rememberMe: boolean
+}
+
 
 const LoginForm = () => {
+    const {
+        register,
+        formState: {
+            errors,
+            isValid
+        },
+        handleSubmit,
+        reset
+    } = useForm({
+        mode: "onBlur"
+    })
+
+    const onSubmit = (data: any) => {
+        alert(JSON.stringify(data))
+        reset()
+    }
+
     return (
-            <form>
-                <div>
-                    <input placeholder={"Login"}/>
-                </div>
-                <div>
-                    <input placeholder={"Password"}/>
-                </div>
-                <div>
-                    <input type={"checkbox"}/> remember me
-                </div>
-                <div>
-                    <button>Login</button>
-                </div>
-            </form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                <input
+                    {...register('login', {
+                        required: "Поле обязательно к заполнению",
+                        minLength: {
+                            value: 5,
+                            message: "Минимум 5 символов"
+                        },
+
+                    })}
+                    placeholder={"Login"}
+                />
+            </label>
+            <div>
+                {errors.login && <span>{errors?.login?.message
+                    ? errors.login.message : 'error!'
+                }</span>}
+            </div>
+
+            <label>
+                <input
+                    {...register('password', {
+                        required: "Поле обязательно к заполнению",
+                        minLength: {
+                            value: 5,
+                            message: "Минимум 5 символов"
+                        }
+                    })}
+                    placeholder={"Password"}
+                />
+            </label>
+            <div>
+                {errors.password && <span>{errors?.password?.message
+                    ? errors.password.message : 'error!'
+                }</span>}
+            </div>
+            <div>
+                <label>Remember me <input type="checkbox" {...register('rememberMe')}/></label>
+            </div>
+            <input type="submit" disabled={!isValid}/>
+
+        </form>
     );
 };
-
 
 
 const Login = () => {
     return (
         <div>
-           <h1>LOGIN</h1>
-          <LoginForm/>
+            <h1>LOGIN</h1>
+            <LoginForm/>
         </div>
     );
 };
